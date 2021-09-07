dd0d67b02eb8e1b2a9eb6b950177c6c1e973b458
gabrielnp <gabrielnp.2010@gmail.com>
2021-09-06 23:01:10 -0300

    feat: use api data
    REFERS TO: crashcourse

--- crashcourse/pages/article/[id]/index.js
+++ crashcourse/pages/article/[id]/index.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+
+import { server } from '../../../config'
+
 
 
 const article = ({article}) => {
-    // const router = useRouter()
-    // const {id} = router.query
     return (
         <>
             <h1>{article.title}</h1>
@@ -16,7 +17,7 @@ const article = ({article}) => {
 }
 
 export const getStaticProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
     const article = await res.json()
     return {
         props: {
@@ -26,7 +27,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles`)
     const articles = await res.json()
     const ids = articles.map(article => article.id)
     const paths = ids.map(id => ({params: {id: id.toString()}}))
@@ -36,4 +37,26 @@ export const getStaticPaths = async () => {
         fallback: false
     }
 }
+
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//     const article = await res.json()
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+//     const articles = await res.json()
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 export default article
